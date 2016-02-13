634a6775c50782f114cec2a6fec0227f007e8275
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2016-02-13 00:07:37 -0600

    Managed to pull some data off

--- hello/helloworld.js
+++ hello/helloworld.js
@@ -2,3 +2,21 @@ function sayHello()
 {
     alert("This is an alert window!");
 }
+
+function getREST(url)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send();
+    if(req.readyState == 4 && req.status == "200")
+    {
+        var string = req.responseText;
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(string,"application/xml");
+        return doc;
+    }
+    else
+    {
+        alert("Not found!");
+    }
+};
