fc916e6597b8858abee01a1d88c3294be43ce930
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2016-02-13 03:08:14 -0600

    Added main test

--- webapp/js/fhir.js
+++ webapp/js/fhir.js
@@ -3,17 +3,17 @@
  */
 
 //Returns an XML document for the url
-function getXML(url)
+function getJSON(url)
 {
+    url += "&_format=json";
     var req = new XMLHttpRequest();
     req.open("GET", url, false);
     req.send();
     if(req.readyState == 4 && req.status == "200")
     {
         var string = req.responseText;
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(string,"application/xml");
-        return doc;
+        var jason= JSON.parse(string);
+        return jason;
     }
     else
     {
@@ -21,7 +21,32 @@ function getXML(url)
     }
 };
 
-function search()
+//Finds patient via ID 
+//Returns patient resource JSON object
+function searchPatientID(url,patientID)
 {
-
+    var entry = getJSON(url+"/Patient/_search?_id="+patientID).entry;
+    if(entry != null)
+    {
+        if(entry[0] != null)
+            return entry[0].resource;
+    }
+    return null
 };
+
+//Finds all observations for patient via ID
+//Returns array of observations
+function searchObservationsPatientID(url,patientID)
+{
+    var entry = getJSON(url+"/Observation/_search?patient="+patientID).entry;
+    var output = [];
+    if(entry != null)
+    {
+        for(i = 0; i < entry.length; i++)
+        {
+            output.push(entry[i].resource);
+        }
+        return output;
+    }
+    return null
+}
