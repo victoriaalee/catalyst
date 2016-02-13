d81b8a31d373147e578d8348f5ba8379cf6fdd63
Victoria Lee <victorialee@utexas.edu>
2016-02-13 15:56:48 -0600

    Implement search

--- webapp/js/fhir.js
+++ webapp/js/fhir.js
@@ -92,3 +92,45 @@ function searchResourcePatient(url,resource,patient)
 {
 	return searchResourcePatientID(url,resource,patient.id);
 }
+
+function filter(jsonData,content,query) {
+	results = new Set();
+	exclude = new Set();
+	query = query.toLowerCase();
+	queryArray = query.split(", ");
+
+	for (int j = 0; j < query.length(); j++) {
+		if (query[j].indexOf("&") != -1) {
+			newQuery = query[j].split(" & ");
+			results = containsWord(jsonData,content,newQuery,results);
+		} else if (query[j].charAt(0) == '-') {
+			newQuery = query[j].substring(1);
+			exclude = containsWord(jsonData,content,newQuery,exclude);
+		}
+		else {
+			newQuery = containsWord(jsonData,content,newQuery,results);
+		}
+	}
+
+	results = containsWord(jsonData,content,word,results);
+	results = results.filter( function(el) { return toRemove.indexOf(exclude) < 0; } );		// remove nots
+	return results;
+}
+
+function containsWord(jsonData,content,query,results) {
+	for (i = 0; i < jsonData.length(); i++) {
+		tmp = true;
+		for (j = 0; j < query.length(); j++) {
+			pattern = new RegExp(query[j]);
+			if (!pattern.test(content[i]).toLowerCase()) {
+				tmp = false;
+				break;
+			}
+		}
+		if (tmp) {
+			results.add(jsonData[i]);
+		}
+	}
+
+	return results;
+}
